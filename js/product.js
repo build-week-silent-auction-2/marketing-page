01510639ab834c2f6d8182a0f2c46de506d61d66
mgroff1 <mick.groff@gmail.com>
2019-11-14 01:13:44 -0700

    Working-out-design

--- js/product.js
+++ js/product.js
@@ -1,24 +1,34 @@
 
-$(document).ready(function () {
-    if (!$.browser.webkit) {
-        $('.wrapper').html('<p>Sorry! Non webkit users. :(</p>');
+
+
+// function open(){
+//     $('.slide').hide("fade",200);
+// $('#cover').show( "slide", { direction: "up"  }, 500 );
+// contain.show( "slide", { direction: "up"  }, 500 );
+//     arr.forEach(function (el) {
+//         el.hide( "slide", { direction: "down"  }, 500 );
+//     });
+
+//     let clicked = $(this);
+//     for(let i =0; i < 4; i++){
+
+//         if(clicked.data("match") === arr[i].data("match")){
+//             arr[i].show( "slide", { direction: "up"  }, 500 );
+//         }
+//    }}
+
+
+//    let upcoming = $('#upcoming');
+//    upcoming.click(function(){
+//     event.preventDefault();
+     
+//        newLocation = 'http://apidocjs.com';
+//        newpage();
+//    })
+
+//    function newpage() {
+
+//     window.location = newLocation;
+    
     }
-});
-
-
-
-function open(){
-    $('.slide').hide("fade",200);
-$('#cover').show( "slide", { direction: "up"  }, 500 );
-contain.show( "slide", { direction: "up"  }, 500 );
-    arr.forEach(function (el) {
-        el.hide( "slide", { direction: "down"  }, 500 );
-    });
-
-    let clicked = $(this);
-    for(let i =0; i < 4; i++){
-
-        if(clicked.data("match") === arr[i].data("match")){
-            arr[i].show( "slide", { direction: "up"  }, 500 );
-        }
-   }}
+    
