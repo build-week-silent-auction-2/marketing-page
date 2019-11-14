8729c0fb105fbabfa8fed8d1197cddd288f07f51
mgroff1 <mick.groff@gmail.com>
2019-11-14 12:30:42 -0700

    worked-on-some-js

--- js/product.js
+++ js/product.js
@@ -1,5 +1,24 @@
 
 
+let panel = $('.panels');
+let item = $('.items');
+let amount= $('.amt');
+let det = $('.details');
 
 
-    
+amount.attr('value','$');
+
+
+item.on('click',function(){
+    // css('width', '40vw', 'height', '40vh','position','absolute');
+    // $(this).parent().toggleClass('details');
+    // $(this).toggleClass('details');
+    $('.details').toggle('slow');
+})
+
+det.on('click',function(){
+    $('.details').toggle('slow');
+})
+
+
+
