ca4ef43a5ae458fb3158f54f3d1db86b323023fd
mgroff1 <mick.groff@gmail.com>
2019-11-14 17:27:25 -0700

    functionality

--- js/product.js
+++ js/product.js
@@ -4,16 +4,20 @@ let panel = $('.panels');
 let item = $('.items');
 let amount= $('.amt');
 let det = $('.details');
-
+let imgArr =[];
 
 amount.attr('value','$');
 
 
 item.on('click',function(){
-    // css('width', '40vw', 'height', '40vh','position','absolute');
-    // $(this).parent().toggleClass('details');
-    // $(this).toggleClass('details');
-    $('.details').toggle('slow');
+    let eleParent = $(this).parent();
+    // eleParent.addClass('.details').css('display','block');
+    // let clicked = $(this);
+    // let detClone = $('.details').clone(true);
+    // console.log(clicked + " " + detClone);
+    // $('.details').toggle('slow');
+    // detClone.append(clicked);
+    
 })
 
 det.on('click',function(){
